0d69b9535e3cb93d47b550f01b6b6d1c0877d291
Sazidul0 <sazidulislam61@gmail.com>
2024-08-21 20:37:03 +0600

    Carousel & Card added in Home page

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,25 +1,61 @@
 import React from 'react';
+import banner1 from '../../Assets/Photo/skill_share-01.jpg'
+import banner2 from '../../Assets/Photo/skill_share-02.jpg'
+import banner3 from '../../Assets/Photo/skill_share-03.jpg'
 
 const Banner = () => {
-    return (
+
+    const bannerDesign = <>
         <div>
-            <div
-                className="hero min-h-screen"
-                style={{
-                    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-                }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-neutral-content text-center">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
+            {/* Extra design */}
+            <div className="hero-overlay"></div>
+            <div className="hero-content text-neutral-content text-center">
+                <div className="max-w-md text-black">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
+        </div>
+    </>
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img
+                    src={banner1}
+                    className="w-full opacity-40" />
+
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between hero min-h-screen">
+
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    {bannerDesign}
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full ">
+                <img
+                    src={banner2}
+                    className="w-full opacity-40" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between hero min-h-screen">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    {bannerDesign}
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img
+                    src={banner3}
+                    className="w-full opacity-40" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between hero min-h-screen">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    {bannerDesign}
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+
         </div>
     );
 };
---
+++ src/Pages/Home/Service.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div className='flex justify-center mt-20'>
+
+
+            <div className=''>
+                <h3 className='text-5xl font-bold flex justify-center mb-6'>Our Services</h3>
+                <div className='all_cards grid grid-cols-1 sm:grid-cols-3 gap-10'>
+
+                    <div className=''>
+                        <div className="card bg-base-100 w-96 shadow-xl">
+                            <figure>
+                                <img
+                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                    alt="Shoes" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div className="card bg-base-100 w-96 shadow-xl">
+                            <figure>
+                                <img
+                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                    alt="Shoes" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div className="card bg-base-100 w-96 shadow-xl">
+                            <figure>
+                                <img
+                                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                    alt="Shoes" />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    Shoes!
+                                    <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div></div>
+                    <div></div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
